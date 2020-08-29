9216b2f88ec447c6ebbfb04dda2a5b597d3c52f3
Ants <code.herder@recombulator.net>
2020-08-29 02:14:06 -0400

    Initial version

---
+++ Passphrase/FairDice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Passphrase
+{
+    class FairDice
+    {
+        static RNGCryptoServiceProvider _rngCsp = new RNGCryptoServiceProvider();
+
+        public static byte Roll(byte sideCount = 6)
+        {
+            if (sideCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(sideCount));
+
+            int fullSetsOfValues = Byte.MaxValue / sideCount;
+            var randomNumber = new byte[1];
+            do
+            {
+                _rngCsp.GetBytes(randomNumber);
+            } while (!IsFairRoll(randomNumber[0]));
+            return (byte)((randomNumber[0] % sideCount) + 1);
+
+            bool IsFairRoll(byte roll)
+                => roll < sideCount * fullSetsOfValues;
+        }
+    }
+}
--- Passphrase/Program.cs
+++ Passphrase/Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace Passphrase
 {
     class Program
     {
+        const string DefaultWordListKey = "large";
+
+        class Config
+        {
+            public string Resource { get; set; }
+            public int DiceCount { get; set; }
+            public int WordCount { get; set; }
+        }
+
+        static Dictionary<string, Config> _wordListConfig = new Dictionary<string, Config>()
+        {
+            ["large"] = new Config() 
+            {
+                Resource = "Passphrase.EFFLargeWordlist.txt",
+                DiceCount = 5,
+                WordCount = 6,
+            },
+            ["short"] = new Config()
+            {
+                Resource = "Passphrase.EFFShortWordlist1.txt",
+                DiceCount = 4,
+                WordCount = 4,
+            },
+            ["memorable"] = new Config()
+            {
+                Resource = "Passphrase.EFFShortWordlist2.txt",
+                DiceCount = 4,
+                WordCount = 4,
+            },
+        };
+
+        static Config GetConfig(string [] args)
+        {
+            string key = (args.Length > 0 ? args[0] : DefaultWordListKey).ToLower();
+
+            if (!_wordListConfig.ContainsKey(key))
+                key = DefaultWordListKey;
+            return _wordListConfig[key];
+        }
+
+        static IEnumerable<(string diceRoll, string word)> LoadWordList(string resourceName)
+        {
+            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(stream);
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var parts = line.Split('\t');
+                if (parts.Length == 2)
+                    yield return (parts[0].Trim(), parts[1].Trim());
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = GetConfig(args);
+            var wordList = LoadWordList(config.Resource).ToDictionary(kvp => kvp.diceRoll, kvp => kvp.word);
+            var words = Enumerable.Repeat(0, config.WordCount)
+                                  .Select(i => GetDiceRoll(config.DiceCount))
+                                  .Select(diceRoll => wordList[diceRoll]);
+            Console.WriteLine(string.Join(" ", words));
+
+            string GetDiceRoll(int diceCount)
+                => string.Join("", Enumerable.Repeat(0, diceCount).Select(n => FairDice.Roll()));
         }
     }
 }
