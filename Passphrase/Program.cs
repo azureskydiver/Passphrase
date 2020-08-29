a3c7ab17021da6929bc411c0c459759d549512ce
Ants <code.herder@recombulator.net>
2020-08-29 00:16:40 -0400

    Add project files.

---
+++ Passphrase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Passphrase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
