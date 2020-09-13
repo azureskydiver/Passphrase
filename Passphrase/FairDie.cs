1adf5b25cc7bf52915784f9e68113115a79b403a
Ants <code.herder@recombulator.net>
2020-09-13 16:06:19 -0400

    Use proper term: die

---
+++ Passphrase/FairDie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine.Rendering.Views;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Passphrase
+{
+    class FairDie<T>
+    {
+        static RNGCryptoServiceProvider _rngCsp = new RNGCryptoServiceProvider();
+
+        readonly List<T> _items;
+        readonly long _maxRange;
+        readonly byte[] _buffer = new byte[sizeof(uint)];
+
+        public FairDie(IEnumerable<T> items)
+        {
+            _items = items.ToList();
+            if (_items.Count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(items));
+
+            var bucketSize = uint.MaxValue / _items.Count;
+            _maxRange = bucketSize * _items.Count;
+        }
+
+        public int RollInt()
+        {
+            uint roll;
+            do
+            {
+                _rngCsp.GetBytes(_buffer);
+                roll = BitConverter.ToUInt32(_buffer);
+            } while (roll >= _maxRange);
+            return (int)(roll % _items.Count);
+        }
+
+        public T Roll() => _items[RollInt()];
+    }
+
+    class FairIntDie : FairDie<int>
+    {
+        public FairIntDie(int sides = 6)
+            : base(Enumerable.Range(1, sides))
+        {
+        }
+    }
+}
